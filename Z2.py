df04bd9e4b88d3ecce5a241a2fa492c1055bea74
niko <socohev@gmail.com>
2022-12-17 18:40:50 +0300

    fin

--- Z2.py
+++ Z2.py
@@ -7,10 +7,13 @@
 # - [2, 3, 5, 6] => [12, 15]
 
 lst = list(map(int, input("Введите числа через пробел:\n").split()))
-l = len(lst)//2 + 1
-if len(lst) % 2 != 1:
-    len(lst)//2
+l = 0
+if len(lst) % 2 != 0:
+    l = len(lst)//2 + 1
+    new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
+    print(new_lst) 
+else:
+    l = len(lst)//2
     new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
     print(new_lst)
-
 
